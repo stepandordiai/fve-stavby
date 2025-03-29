02a6e5ef73363980be185820afb7668cf737478c
Stepan Dordiai <stepandordiai@gmail.com>
2025-03-29 13:26:11 +0100

    v2.7.1 Update Partners

--- src/components/Partners/Partners.jsx
+++ src/components/Partners/Partners.jsx
@@ -1,11 +1,16 @@
-import logo from "./../../assets/partnersLogos/solax.png";
-import logo1 from "./../../assets/partnersLogos/deye.png";
+import partner1 from "./../../assets/partnersLogos/aiko.png";
+import partner2 from "./../../assets/partnersLogos/canadian-solar.webp";
+import partner3 from "./../../assets/partnersLogos/deye.png";
+import partner4 from "./../../assets/partnersLogos/jinko.png";
+import partner5 from "./../../assets/partnersLogos/solax.png";
+import partner6 from "./../../assets/partnersLogos/tigo.png";
+
 import "./Partners.scss";
 
 const Partners = () => {
     return (
         <>
-            <h2 className="partners-title">Nasi partneri</h2>
+            <h2 className="partners-title">Naši partneři</h2>
             <p>
                 Spolupracujeme s prvotřídními výrobci, dodavateli, firmami a
                 institucemi z České republiky a zahraničí.
@@ -16,18 +21,41 @@ const Partners = () => {
                 style={{
                     "--width": "100px",
                     "--height": "50px",
-                    "--quantity": "2",
+                    "--quantity": "6",
                 }}
             >
                 <div className="list">
                     <div className="item" style={{ "--position": "1" }}>
-                        <a href="https://us.solaxpower.com/" target="_blank">
-                            <img src={logo} alt="" />
+                        <a href="https://aikosolar.com/cz/" target="_blank">
+                            <img src={partner1} alt="Partner Logo" />
                         </a>
                     </div>
                     <div className="item" style={{ "--position": "2" }}>
-                        <a href="https://deye.com/cs/" target="_blank">
-                            <img src={logo1} alt="" />
+                        <a
+                            href="https://www.canadiansolar.com/"
+                            target="_blank"
+                        >
+                            <img src={partner2} alt="Partner Logo" />
+                        </a>
+                    </div>
+                    <div className="item" style={{ "--position": "3" }}>
+                        <a href="https://deye.com/" target="_blank">
+                            <img src={partner3} alt="Partner Logo" />
+                        </a>
+                    </div>
+                    <div className="item" style={{ "--position": "4" }}>
+                        <a href="https://jinkosolar.eu/" target="_blank">
+                            <img src={partner4} alt="Partner Logo" />
+                        </a>
+                    </div>
+                    <div className="item" style={{ "--position": "5" }}>
+                        <a href="https://www.solaxpower.com/" target="_blank">
+                            <img src={partner5} alt="Partner Logo" />
+                        </a>
+                    </div>
+                    <div className="item" style={{ "--position": "6" }}>
+                        <a href="https://www.tigoenergy.com/" target="_blank">
+                            <img src={partner6} alt="Partner Logo" />
                         </a>
                     </div>
                 </div>
