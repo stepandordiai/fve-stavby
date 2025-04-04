34510b8907e2121fc2c36d6b63314297bba8d9e5
Stepan Dordiai <stepandordiai@gmail.com>
2025-04-04 14:52:10 +0200

    v2.11.0 Add fullScreen container for OurInstallations and fix bugs

--- src/pages/OurInstallation/OurInstallation.jsx
+++ src/pages/OurInstallation/OurInstallation.jsx
@@ -8,9 +8,111 @@ import installationImg3 from "/assets/installations/3.jpg";
 import installationImg4 from "/assets/installations/4.jpg";
 import installationImg5 from "/assets/installations/5.jpg";
 import installationImg6 from "/assets/installations/6.jpg";
+import { useEffect } from "react";
+import closeIcon from "/assets/icons/close-white.png";
+import expandIcon from "/assets/icons/expand-white.png";
+import shrinkIcon from "/assets/icons/shrink-white.png";
 import "./OurInstallation.scss";
 
 const OurInstallation = () => {
+    const imgData = [
+        {
+            id: 1,
+            img: installationImg1,
+        },
+        {
+            id: 2,
+            img: installationImg2,
+        },
+        {
+            id: 3,
+            img: installationImg3,
+        },
+        {
+            id: 4,
+            img: installationImg4,
+        },
+        {
+            id: 5,
+            img: installationImg5,
+        },
+        {
+            id: 6,
+            img: installationImg6,
+        },
+    ];
+
+    useEffect(() => {
+        document
+            .querySelectorAll(".our-installation__card img")
+            .forEach((img, index) => {
+                img.addEventListener("click", () => {
+                    if (Number(img.dataset.value) == imgData[index].id) {
+                        // Create elements
+
+                        const fullScreen = document.createElement("div");
+                        const fullScreenHeader =
+                            document.createElement("header");
+                        const fullScreenCloseBtn =
+                            document.createElement("button");
+                        const fullScreenImage = document.createElement("img");
+                        const fullScreenFooter =
+                            document.createElement("footer");
+                        const fullScreenBtn = document.createElement("button");
+
+                        // Add classNames for elements
+
+                        fullScreen.classList.add("full-screen-div");
+                        fullScreenHeader.classList.add("full-screen-header");
+                        fullScreenCloseBtn.classList.add(
+                            "full-screen__close-btn"
+                        );
+                        fullScreenImage.classList.add("full-screen-image");
+                        fullScreenFooter.classList.add("full-screen-footer");
+                        fullScreenBtn.classList.add("full-screen__btn");
+
+                        fullScreenCloseBtn.innerHTML = `<img title="Close" src="${closeIcon}" alt="Close icon" loading="lazy" />`;
+                        fullScreenImage.src = imgData[index].img;
+                        fullScreenBtn.innerHTML = `<img title="Expand" src="${expandIcon}" alt="Expand icon" loading="lazy" />`;
+
+                        fullScreenCloseBtn.addEventListener("click", () => {
+                            fullScreen.remove();
+                        });
+                        fullScreenBtn.addEventListener("click", () => {
+                            if (
+                                fullScreenBtn
+                                    .querySelector("img")
+                                    .src.includes("expand")
+                            ) {
+                                if (fullScreen.requestFullScreen) {
+                                    fullScreen.requestFullScreen();
+                                } else if (fullScreen.mozRequestFullScreen) {
+                                    fullScreen.mozRequestFullScreen();
+                                } else if (fullScreen.webkitRequestFullScreen) {
+                                    fullScreen.webkitRequestFullScreen();
+                                }
+                                fullScreenBtn.innerHTML = `<img title="Shrink" src="${shrinkIcon}" alt="Shrink icon" loading="lazy" />`;
+                            } else {
+                                document.exitFullscreen();
+                                fullScreenBtn.innerHTML = `<img title="Expand" src="${expandIcon}" alt="Expand icon" loading="lazy" />`;
+                            }
+                        });
+
+                        // Appending elements
+
+                        document.body.appendChild(fullScreen);
+                        fullScreen.append(
+                            fullScreenHeader,
+                            fullScreenImage,
+                            fullScreenFooter
+                        );
+                        fullScreenHeader.appendChild(fullScreenCloseBtn);
+                        fullScreenFooter.appendChild(fullScreenBtn);
+                    }
+                });
+            });
+    }, []);
+
     return (
         <>
             <Helmet>
@@ -27,7 +129,7 @@ const OurInstallation = () => {
                 <div className="our-installation__grid">
                     <div className="our-installation__card-wrapper">
                         <div className="our-installation__card">
-                            <img src={installationImg1} alt="" />
+                            <img src={installationImg1} data-value="1" alt="" />
                             <div className="white-el"></div>
                             <p className="our-installation__card-date-info">
                                 <span>Instalováno 18.12.2024</span>
@@ -39,7 +141,7 @@ const OurInstallation = () => {
                     </div>
                     <div className="our-installation__card-wrapper">
                         <div className="our-installation__card">
-                            <img src={installationImg2} alt="" />
+                            <img src={installationImg2} data-value="2" alt="" />
                             <div className="white-el"></div>
                             <p className="our-installation__card-date-info">
                                 <span>Instalováno 18.12.2024</span>
@@ -51,7 +153,7 @@ const OurInstallation = () => {
                     </div>
                     <div className="our-installation__card-wrapper">
                         <div className="our-installation__card">
-                            <img src={installationImg3} alt="" />
+                            <img src={installationImg3} data-value="3" alt="" />
                             <div className="white-el"></div>
                             <p className="our-installation__card-date-info">
                                 <span>Instalováno 18.12.2024</span>
@@ -63,7 +165,7 @@ const OurInstallation = () => {
                     </div>
                     <div className="our-installation__card-wrapper">
                         <div className="our-installation__card">
-                            <img src={installationImg4} alt="" />
+                            <img src={installationImg4} data-value="4" alt="" />
                             <div className="white-el"></div>
                             <p className="our-installation__card-date-info">
                                 <span>Instalováno 18.12.2024</span>
@@ -75,7 +177,7 @@ const OurInstallation = () => {
                     </div>
                     <div className="our-installation__card-wrapper">
                         <div className="our-installation__card">
-                            <img src={installationImg5} alt="" />
+                            <img src={installationImg5} data-value="5" alt="" />
                             <div className="white-el"></div>
                             <p className="our-installation__card-date-info">
                                 <span>Instalováno 18.12.2024</span>
@@ -87,7 +189,7 @@ const OurInstallation = () => {
                     </div>
                     <div className="our-installation__card-wrapper">
                         <div className="our-installation__card">
-                            <img src={installationImg6} alt="" />
+                            <img src={installationImg6} data-value="6" alt="" />
                             <div className="white-el"></div>
                             <p className="our-installation__card-date-info">
                                 <span>Instalováno 18.12.2024</span>
