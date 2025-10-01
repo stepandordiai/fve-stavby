fc24d9e580af9693abb27a4c5caf6bfa959bcb6f
Stepan Dordiai <stepandordiai@gmail.com>
2025-10-01 15:19:22 +0200

    Update Header with burger btn

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,4 @@
 import { useTranslation } from "react-i18next";
-import BurgerBtn from "../BurgerBtn/BurgerBtn";
 import { NavLink, useLocation } from "react-router-dom";
 import LngSelect from "../LngSelect/LngSelect";
 import { useEffect } from "react";
@@ -118,6 +117,28 @@ const Header = () => {
 		// };
 	}, []);
 
+	function toggleBurgerBtn(): void {
+		const burgerBtn = document.querySelector(
+			".burger-btn"
+		) as HTMLDivElement | null;
+		const burgerBtnCenterLine = document.querySelector(
+			".burger-btn__center-line"
+		) as HTMLSpanElement | null;
+		const menu = document.querySelector(".menu") as HTMLDivElement | null;
+		const menuWrapper = document.querySelector(
+			".menu-wrapper"
+		) as HTMLDivElement | null;
+		const headerBottom = document.querySelector(
+			".header-bottom"
+		) as HTMLDivElement | null;
+
+		burgerBtn?.classList.toggle("burger-btn--active");
+		burgerBtnCenterLine?.classList.toggle("burger-btn__center-line--active");
+		menu?.classList.toggle("menu--active");
+		menuWrapper?.classList.toggle("menu-wrapper--active");
+		headerBottom?.classList.toggle("header-bottom--active");
+	}
+
 	return (
 		<>
 			<header className="header">
@@ -131,7 +152,11 @@ const Header = () => {
 							+420 728 803 703
 						</a>
 						<LngSelect />
-						<BurgerBtn />
+						<div onClick={toggleBurgerBtn} className="burger-btn-wrapper">
+							<div className="burger-btn">
+								<span className="burger-btn__center-line"></span>
+							</div>
+						</div>
 					</div>
 					<div className="header-bottom-wrapper">
 						<nav className="header-bottom">
