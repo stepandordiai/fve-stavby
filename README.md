fde7e58eb26782dad2caa42567bbe57231375c5b
Stepan Dordiai <stepandordiai@gmail.com>
2025-11-12 23:53:33 +0100

    Update Home page and README

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 March 2025
 
+## Description
+
 I designed and developed a multi-language website for a solar panel installation service provider. The site is built with modern web technologies, optimized for performance and SEO, and features service pages for both residential and commercial customers. I implemented dynamic content using i18next for localization and ensured responsiveness across all devices.
 
 ![FVE STAVBY hero screenshot](./public/screenshot/fve-stavby-ss-v7-c.png)
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,11 +4,13 @@ import Process from "../../components/Process/Process";
 import WhyUs from "../../components/WhyUs/WhyUs";
 import Partners from "../../components/Partners/Partners";
 import Stats from "../../components/Stats/Stats";
-import HomeTop from "../../components/HomeTop/HomeTop";
+import { NavLink } from "react-router-dom";
 import HomeAboutCompany from "../../components/HomeAboutCompany/HomeAboutCompany";
 import GetInTouch from "../../components/GetInTouch/GetInTouch";
 import WhySolar from "../../components/WhySolar/WhySolar";
 import Container from "../../components/Container/Container";
+import arrow from "/icons/arrow-upper-right-white.png";
+import img from "/img/01-c.jpg";
 import "./Home.scss";
 
 const Home = () => {
@@ -23,7 +25,29 @@ const Home = () => {
 			</Helmet>
 			<main>
 				<Container>
-					<HomeTop />
+					<section className="home-hero">
+						<div className="home-top">
+							<img className="home-top__img" src={img} alt="" />
+							<p className="loading__title">FVE STAVBY</p>
+							<div className="home-top__details-container">
+								<div className="home-top__details">
+									<h1 className="home-top__title">FVE - stavby s.r.o.</h1>
+									<h2 className="home-top__sec-title">{t("logo_title")}</h2>
+									<h2 className="home-top__info">{t("home.title")}</h2>
+								</div>
+								<div className="home-top__link-container">
+									<NavLink className="home-top__link" to="/manuals">
+										<span>{t("manuals_title")}</span>
+										<img className="home-top__link-icon" src={arrow} alt="" />
+									</NavLink>
+									<NavLink className="home-top__link" to="/contacts">
+										<span>{t("home.contacts_link")}</span>
+										<img className="home-top__link-icon" src={arrow} alt="" />
+									</NavLink>
+								</div>
+							</div>
+						</div>
+					</section>
 					<div className="home">
 						<HomeAboutCompany />
 						<WhyUs />
