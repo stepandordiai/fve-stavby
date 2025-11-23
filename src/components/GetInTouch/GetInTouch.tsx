76c6661a075b2f9d11c34a532e80f28263ee1385
Stepan Dordiai <stepandordiai@gmail.com>
2025-11-23 11:25:47 +0100

    Update GetInTouch with new svg icon

--- src/components/GetInTouch/GetInTouch.tsx
+++ src/components/GetInTouch/GetInTouch.tsx
@@ -1,7 +1,6 @@
 import { useTranslation } from "react-i18next";
 import { NavLink } from "react-router-dom";
 import img from "/img/06-c.jpg";
-import arrowIcon from "/icons/arrow-upper-right-white.png";
 import "./GetInTouch.scss";
 
 const GetInTouch = () => {
@@ -11,9 +10,23 @@ const GetInTouch = () => {
 		<div className="get-in-touch">
 			<img className="get-in-touch__bg" src={img} alt="" loading="lazy" />
 			<p className="get-in-touch__title">{t("get_in_touch_title")}</p>
-			<NavLink to={"/contacts"} className="get-in-touch__link">
+			<NavLink to="/contacts" className="get-in-touch__link">
 				<span>{t("get_in_touch_link")}</span>
-				<img className="get-in-touch__link-icon" src={arrowIcon} alt="" />
+				<svg
+					width="32"
+					height="32"
+					viewBox="0 0 24 24"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path
+						d="M7 17L17 7M17 7H8M17 7V16"
+						stroke="currentColor"
+						stroke-width="1"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					/>
+				</svg>
 			</NavLink>
 		</div>
 	);
